719dfa95eeb17ffb3d2dba083b4734ae511d6053
Viktor Volynetz <volynets.viktor@gmail.com>
2021-02-04 10:07:30 +0200

    Added  My Select html helper

--- WebUniversity/Models/Student.cs
+++ WebUniversity/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace WebUniversity
     public class Student
     {
         public int Id { set; get; }
+        [Display(Name = "Student Name")]
         public string Name { set; get; }
         //
         public virtual ICollection<Discipline> Disciplines { set; get; }
